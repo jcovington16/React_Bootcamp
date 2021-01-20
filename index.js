89222117b4999bec042b80aac9a4695bf3fd39b1
jcybertech <covingtonjoshua7@gmail.com>
2021-01-19 18:22:51 -0700

    Understanding the difference between Class/Function Components.

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-class Hello extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-			</div>
-		);
-	}
+// class Hello extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// Understanding the difference between Class/Function Components
+function Hello() {
+  return (
+    <div>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+    </div>
+  )
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'))
