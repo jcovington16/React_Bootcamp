5bdc8eff1ffbe88476aee1c223a8e209191b2334
jcovington16 <covingtonjoshua7@gmail.com>
2021-01-30 21:11:49 -0700

    logged the content to the console. We can see the random fruit picked with messages below it. Every time we refresh the page we get a new fruit.

--- fruits_exercise/src/index.js
+++ fruits_exercise/src/index.js
@@ -1,2 +1,20 @@
 import foods from './foods'
 import { choice, remove } from './helpers'
+
+// Randomly draw a fruit from array
+let fruit = choice(foods)
+
+// Log the message "I'd like on RANDOMFRUIT please."
+console.log(`I'd like one ${fruit}, please.`)
+
+// Log the message "Here you go: RANDOMFRUIT"
+console.log(`Here you go: ${fruit}`)
+
+// Log the message "Delicious! May I have another?"
+console.log('Delicious! May I have another?')
+
+// Remove the fruit from the array of fruits
+let remaining = remove(foods, fruit)
+
+// Log the message "I'm sorry, we're all out. We have FRUITSLEFT left."
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`)
